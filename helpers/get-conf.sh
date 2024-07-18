9987020f4f9102463f9e12cb8c71697c1783a788
Judit Novak <judit.novak@canonical.com>
2024-07-18 13:16:26 +0200

    Using yq to fetch config values (as suggested by @medib)
    Co-authored-by: Mehdi Bendriss <bendrissmehdi@gmail.com>

--- helpers/get-conf.sh
+++ helpers/get-conf.sh
@@ -6,12 +6,5 @@ function get_yaml_prop() {
     local full_key_path="${2}"
     local default="${3}"
 
-    value=$( cat $target_file | grep -v "^#" | grep $full_key_path | tr -s [:blank:] | cut -d' ' -f 2 )
-
-    if [ $value ]
-    then
-        echo $value
-        return
-    fi
-    echo $default
+   "${SNAP}"/bin/yq ".\"${full_key_path}\" // \"${default}\"" "${target_file}"
 }
